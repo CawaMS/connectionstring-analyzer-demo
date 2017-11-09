fee56e443f628b6d3e2de8f06202190bb7516fef
Catherine Wang <cawa@microsoft.com>
2017-11-09 15:28:15 -0800

    project that contains fake connection string for demo purpose

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.WindowsAzure.Storage;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=serverhosting;AccountKey=3lRDKjPR9i9Z+tJHtKG8ahgUX16wCNr2mHQgr8Jrdx5zONyTnx6jPwytDincVqsT8HSOwU3fprHCUVJbl/Tirw==");
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
